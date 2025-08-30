e068286c8e308871b64e16bef440fceb162249ac
Mikumisquipasse <167207761+journaldegermain@users.noreply.github.com>
2025-08-30 15:34:54 +0200

    data.js pour la version 0.1

---
+++ data.js
@@ -0,0 +1,38 @@
+const maisons = [
+  {
+    id: 1,
+    nom: "Maison moderne",
+    region: "Ville",
+    photos: ["images/ville1_1.png", "images/ville1_2.png", "images/ville1_3.png"],
+    prix: ["5 émeraudes", "1 Elytra"],
+    vendu: false,
+    visiteEnCours: false
+  },
+  {
+    id: 2,
+    nom: "Temple Japonais",
+    region: "Quartier Japonais",
+    photos: ["images/japon1_1.png","images/japon1_2.png"],
+    prix: ["3 stacks de bambous","1 Trident"],
+    vendu: false,
+    visiteEnCours: false
+  },
+  {
+    id: 3,
+    nom: "Maison rustique",
+    region: "Cobble City",
+    photos: ["images/cobble1_1.png","images/cobble1_2.png"],
+    prix: ["10 stacks de cobble","1 enclume"],
+    vendu: false,
+    visiteEnCours: false
+  },
+  {
+    id: 4,
+    nom: "Ferme champêtre",
+    region: "Campagne",
+    photos: ["images/campagne1_1.png","images/campagne1_2.png"],
+    prix: ["2 chevaux","64 blés"],
+    vendu: false,
+    visiteEnCours: false
+  }
+];
